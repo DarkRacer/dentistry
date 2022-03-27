724910177b756305328ae4b43c8fc4e6886b87b9
DarkRacer <schtscherbakov@gmail.com>
2022-03-27 19:20:57 +0300

    Add project structure

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Стоматология");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 338, 174));
         primaryStage.show();
     }
 
